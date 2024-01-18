d7dbe4b7eda2d0a7e220a2b7c92887518d177789
Matthias Harzer <matthias.harzer03@gmail.com>
2024-01-18 23:49:45 +0100

    Added dynamic font size font every screen size

--- src/lib/VideoPlayer.svelte
+++ src/lib/VideoPlayer.svelte
@@ -12,13 +12,55 @@
     let loading_frames = false;
     let interval_id: number;
     let playing = false;
+    let video_info: AsciiVideoInfoResponse;
     let error: any = null;
+    let frame_width: number | null = null;
+    let line_height: number | null = null;
+    let font_size: number = 10;
+
+    const determine_font_size = () => {
+        if (frame_width == null || line_height == null) {
+            return;
+        }
+
+        font_size = 20;
+        const viewport_width = window.innerWidth - 50;
+        const viewport_height = window.innerHeight + 30;
+        const element = document.createElement("span");
+        const sample_line = "0".repeat(frame_width);
+
+        element.style.position = "static";
+        element.style.fontSize = font_size + "px";
+        element.style.fontFamily = "monospace";
+
+        element.innerText = sample_line;
+
+        document.body.appendChild(element);
+
+        let {width, height} = element.getBoundingClientRect();
+
+        while ((width > viewport_width || height > viewport_height) && font_size > 1) {
+            font_size -= 1;
+
+            element.style.fontSize = font_size + "px";
+            const rect = element.getBoundingClientRect();
+
+            width = rect.width;
+            height = rect.height * line_height;
+        }
+
+        element.remove();
+
+        console.log(viewport_width, viewport_height, "font size", font_size);
+    }
 
     const play_video = async () => {
 
         if (!video_client || playing) {
             return;
         }
+
+        determine_font_size();
 
         playing = true;
 
@@ -76,6 +118,12 @@
 
         frames = await video_client.get_next_frames(0, fps);
 
+        if (Object.keys(frames).length === 0) {
+            return;
+        }
+        frame_width = frames[0].split("\n")[0].length;
+        line_height = frames[0].split("\n").length;
+
         play_video();
     }
 
@@ -89,7 +137,6 @@
 
     $: play_pause_button_text = playing ? "Pause" : "Play";
 
-    let video_info: AsciiVideoInfoResponse;
 
     $: fps = video_info?.fps;
     $: frames_count = video_info?.frames_count;
@@ -98,6 +145,8 @@
     $: if (video_client) {
         init_video();
     }
+
+    window.addEventListener("resize", determine_font_size);
 </script>
 
 <div class="main">
@@ -108,7 +157,7 @@
         </div>
     {:else}
         <div class="player">
-            <div bind:this={frame_element} class="frame">
+            <div bind:this={frame_element} class="frame" style="--font-size: {font_size}px;">
 
             </div>
             <div class="video-nav">
@@ -133,17 +182,27 @@
     .main {
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        justify-content: center;
         align-items: center;
+    }
+
+    .player{
+        width: auto;
     }
 
     .frame {
         font-family: monospace;
-        font-size: 10px;
+        /*font-size: 10px;*/
+        font-size: var(--font-size);
         white-space: pre;
         height: 100%;
-        overflow: hidden;
+        width: fit-content;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         line-height: 1;
+        flex: 0 1 auto;
     }
 
     .video-nav {
@@ -152,6 +211,7 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+        /*width: 0;*/
     }
 
     .progress-bar {
