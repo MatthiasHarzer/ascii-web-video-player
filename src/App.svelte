5db467a32921472e11251c2a1c0664572a9db89a
Matthias Harzer <matthias.harzer03@gmail.com>
2024-01-27 13:30:14 +0100

    Added github reference

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-
+    import githubLogo from "./assets/github.svg";
     import VideoPlayer from "./lib/VideoPlayer.svelte";
 
     const url = new URL(window.location.href);
@@ -11,7 +11,6 @@
     }
 
 
-
 </script>
 
 <main>
@@ -20,8 +19,36 @@
     {:else}
         <h1>Video not found</h1>
     {/if}
+
+    <div class="git-ref">
+        Check out this project on <a href="https://github.com/MatthiasHarzer/ascii-web-video-player">GitHub
+        <img src={githubLogo} alt="GitHub Logo">
+    </a>
+
+    </div>
 </main>
 
 <style>
+    a {
+        font-weight: 500;
+        color: #646cff;
+        text-decoration: inherit;
+    }
+    a:hover {
+        color: #535bf2;
+    }
+    .git-ref{
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        padding: 10px;
+        font-weight: 500;
+    }
+
+    .git-ref img{
+        height: 1.2em;
+        position: relative;
+        top: 0.2em;
+    }
 
 </style>
